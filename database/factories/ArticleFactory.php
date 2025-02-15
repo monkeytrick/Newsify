d1042cc4d8ce5a00170c1453cb327ee0f303a6f5
monkeytrick <brianstack@hotmail.com>
2025-02-15 20:01:02 +0900

    database tables, factories, seeders and migrations created

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Article;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Article::class;
+
+    public function definition(): array
+    {
+        return [
+            //
+            'publication' => 'The Sun',
+            'country' => 'uk',
+            'title' => 'Big Titties',
+            'views' => 10,
+            'search_type' => 'default'
+        ];
+    }
+}
---
+++ database/migrations/2025_02_15_090800_articles_viewed.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('articles_viewed', function (Blueprint $table) {
+            $table->id();
+            $table->string('publication');
+            $table->string('country');
+            $table->string('title');
+            $table->integer('views');
+            $table->string('search_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::drop('articles_viewed');
+    }
+};
